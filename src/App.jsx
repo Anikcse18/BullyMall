731223cbaff75276875fb2daf2ccfec3fdb605de
Anikcse18 <istiakjavedanik@gmail.com>
2024-08-12 11:38:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./LayoutPage/Footer";
+import Header from "./LayoutPage/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/LayoutPage/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="roboto text-4xl">Header</h1>
+    </div>
+  );
+};
+
+export default Header;
